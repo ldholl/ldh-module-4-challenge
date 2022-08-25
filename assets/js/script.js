1085563ab9aa6195e5da92c5b54af1f8efa681e7
ldholl <lacy.d.holleman@gmail.com>
2022-08-25 14:33:52 -0400

    added function to generate questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,69 +13,99 @@ THEN I can save my initials and score*/
 
 var quizContainer = document.getElementById("quiz-content");
 var beginButton = document.getElementById("begin-btn");
-
-var quizQuestions = [
-    {question:"In what year was JavaScript introduced?",
-     choices: {
-        a: "2001",
-        b: "1995",
-        c: "1993"
-     },
-     correct: "1995"
-    },
-    
-    {question:"Which of the following is known as the 'dot operator?'",
-        choices: {
-           a: ".",
-           b: ":",
-           c: "%"
-        },
-        correct: "."
-       }
-    //question template for copy/pasting
-    /*{question:,
-        answers: {
-           a:,
-           b:,
-           c:,
-        },
-       }*/
-]
-
 var questionCounter = 0;
-var questionArray = [];
-var quiz = document.getElementById("quiz");
+var playerScore = 0;
 
+var quizQuestions = [{
+  question:"In what year was JavaScript introduced?",
+  options: ["2001", "1995", "1993"],
+  correct: 1
+  },
+  
+  {
+  question:"Which of the following is known as the 'dot operator?'",
+  options: [".", ":", "%"],
+  correct: 0
+  }
+];
 
-//Quiz function
-function showQuestion(){
-    console.log("Clicked");
-    //remove intro and begin button
-    var starterBox = document.getElementById("quiz-starter");
-    starterBox.remove();
+//Clear window for quiz
+function startQuiz(){
+console.log("clicked");
 
+var starterBox = document.getElementById("quiz-starter");
+starterBox.remove();
 
-
-    for (i = 0; i < quizQuestions.length; i++){
-
-    }
-};
-
-//Create and add the html to display questions
-var createQuestion = function (){
-    var questionDisplayEl = document.createElement("div");
-    questionDisplayEl.className = "question-display";
-    questionDisplayEl.innerHTML = "<h3 class='question-header'>"+ "Question " + questionCounter + "</h3>"
-
-    quizContainer.append(questionDisplayEl);
-
-
-
+createQuestion();
 }
+//Create new question
+function createQuestion(){
+
+  for (questionCounter; questionCounter < quizQuestions.length; questionCounter++){
+      
+    //create the question header 
+      var questionDisplayEl = document.createElement("div");
+      questionDisplayEl.className = "question-display";
+      questionDisplayEl.innerHTML = "<h3 class='question-header'>"+ "Question " + (questionCounter + 1) + "</h3>";
+
+  //attach question number to html
+      quizContainer.append(questionDisplayEl);
+    
+      var quizIndex = questionCounter;
+      var questionEl = document.createElement("div");
+      questionEl.className = "question-options";
+      questionEl.innerHTML = "<p>" + quizQuestions[quizIndex].question + "</p>";
+  
+      questionDisplayEl.append(questionEl);
+      var btnId = 0;
+          for (var i=0; i < quizQuestions[quizIndex].options.length; i++){
+            
+            var optionBtnEl = document.createElement("button");
+            optionBtnEl.id = btnId;
+            optionBtnEl.onclick = checkAnswer;
+            optionBtnEl.className = "option-button"
+            optionBtnEl.innerText = quizQuestions[quizIndex].options[i];
+            questionDisplayEl.append(optionBtnEl);
+            btnId ++;
+
+           function checkAnswer(){
+              console.log(this.id)
+              console.log(quizQuestions[quizIndex].correct)
+              if (this.id == quizQuestions[quizIndex].correct){
+                playerScore ++;
+                
+                questionEl.innerHTML = "<p id = answer-response> That's Correct! Your score is now " + playerScore + "! </p>"
+              
+              }
+              else {
+               
+                questionEl.innerHTML = "<p id = answer-response> Wrong! Sorry! Your score is now " + playerScore + "</p>";
+                
+              }
+              debugger;
+             
+             
+            }
+
+            
+          };
+          
+      }
+  }
  
+;
 
 
-//Listener for 'begin' button
-beginButton.addEventListener("click", showQuestion)
+
+
+
+
+//buttons.addEventListener("click", checkAnswer);
+
+
+beginButton.addEventListener("click", startQuiz);
+
+
+
 
 
