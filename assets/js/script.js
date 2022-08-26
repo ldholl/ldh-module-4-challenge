0a69d526397f539bc69349cff77d8ae20b7dc213
ldholl <lacy.d.holleman@gmail.com>
2022-08-25 20:44:22 -0400

    finished functions and added minor css

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,6 @@ var timerEl = document.getElementById("timer");
 var seconds;
 
 
-
-
 var quizQuestions = [{
   question:"In what year was JavaScript introduced?",
   options: ["2001", "1995", "1993"],
@@ -32,12 +30,26 @@ var quizQuestions = [{
   question:"Which of the following is known as the 'dot operator?'",
   options: [".", ":", "%"],
   correct: 0
+  },
+
+  {
+    question: "True or False: Function expressions often result in anonymous functions",
+    options: ["True", "False"],
+    correct: 0
+  },
+  {
+    question: "True or False: a symbol is a primitive data type.",
+    options: ["True", "False"],
+    correct: 1
+  },
+  {
+    question: "What purpose does the '!' operator serve?",
+    options: ["! checks whether two things are true", "! checks that at least one of a set of conditions is true", "! negates the value of a boolean"],
+    correct: 2
   }
+
 ];
 
-
-
-//Clear window for quiz
 function startQuiz(){
 console.log("clicked");
 
@@ -57,14 +69,15 @@ function startTimer(){
     if (seconds > 0){
       setTimeout(tick, 1000);
     }
+    else{endGame()}
   }
   tick();
+
 };
 
 //Create new question
 function newQuestion(){
-  //countdown timer
-
+ if (questionCounter < quizQuestions.length){
     //create the question header 
 
       questionDisplayEl.innerHTML = "<h3 class='question-header'>"+ "Question " + (questionCounter + 1) + "</h3>";
@@ -90,9 +103,6 @@ function newQuestion(){
             btnId ++;
 
            function checkAnswer(){
-            
-              console.log(this.id)
-              console.log(quizQuestions[quizIndex].correct)
               if (this.id == quizQuestions[quizIndex].correct){
                 playerScore ++;
                 
@@ -101,11 +111,11 @@ function newQuestion(){
               
               }
               else {
-               
-                questionDisplayEl.innerHTML = "<p id = answer-response> Wrong! Sorry! Your score is now " + playerScore + "</p>";
+                seconds = seconds -5;
+                questionDisplayEl.innerHTML = "<p id = answer-response> Wrong! Sorry! Your score is " + playerScore + "</p>";
                 
               }
-              
+
               questionCounter ++;
 
               var nextButtonEl = document.createElement("button");
@@ -114,10 +124,16 @@ function newQuestion(){
               
               questionDisplayEl.append(nextButtonEl);
 
-             nextButtonEl.addEventListener("click", newQuestion);
+             nextButtonEl.addEventListener("click", newQuestion);}
+
+
+            
+
             }
-          };       
-      }
+            }
+            else{endGame()};
+          };
+      
   
   
 
@@ -127,7 +143,32 @@ function newQuestion(){
 beginButton.addEventListener("click", startQuiz);
 
 
-function endGame(){};
+function endGame(){
 
+
+var highScore = localStorage.getItem("highscore");
+if(highScore === null){
+  highScore = 0;
+}
+//if player has a higher score than the previous saved score
+if (playerScore > highScore){
+  
+  questionDisplayEl.innerHTML = "<p id= new-high-score>You set a new High Score of " + playerScore + "! Please enter your name to save your score.</p>"
+  var playerNameEl = document.createElement("div");
+  playerNameEl.innerHTML = "<input type='text' id='player-name'/><button id='name-submit'>Submit</button>"
+
+
+  questionDisplayEl.append(playerNameEl);
+
+  playerNameEl.addEventListener("click", saveScore)
+
+  function saveScore(){
+    localStorage.setItem("highscore", playerScore); 
+    localStorage.setItem("name", playerNameEl.value);
+  }
+}
+else{
+  questionDisplayEl.innerHTML = "<p>Thank you for playing! Better Luck Next Time</p>"
+}}
 
 
